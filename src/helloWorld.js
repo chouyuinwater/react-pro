d32a6cc4ef9e07e760b6708625f2eb5373948087
bingQ <koorisan@hotmail.com>
2020-10-27 08:59:13 +0800

    hello world

---
+++ src/helloWorld.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function hello() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+}
+
+let Hello = hello;
+
+export default Hello;
