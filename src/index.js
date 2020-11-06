464854f9bef5dc1ed694df151ee63ad861a91751
jiangbing <jiangbing@yuewen.com>
2020-11-06 18:51:28 +0800

    click

--- src/index.js
+++ src/index.js
@@ -4,14 +4,30 @@ import './index.css';
 import App from './App';
 import Hello from './helloWorld';
 import reportWebVitals from './reportWebVitals';
+import {Say, Seed} from './Say';
+import Clock from './Clock';
+let ticks = function tick() {
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-    <Hello />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    ReactDOM.render(
+        <Clock name="小猪"/>,
+        document.getElementById('root')
+    );
+}
+
+setInterval(ticks, 1000);
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//     <Hello />
+//     <Seed name={"小猪"} user={User} />
+//     <Say />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
