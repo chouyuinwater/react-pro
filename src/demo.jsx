ade1ec7ca26500e143a475149bd80f4786eb217a
jiangbing <jiangbing@yuewen.com>
2020-12-25 14:48:59 +0800

    jsx

---
+++ src/demo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Clock extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>guest Hello!!</h1>
+            </div>
+        );
+    }
+}
