ead62eeed9c51ca5c6284976c3c066906adb0b73
jiangbing <jiangbing@yuewen.com>
2021-01-28 20:20:01 +0800

    data

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default class App extends React.Component {
 
     componentDidMount() {
-        fetch("http://localhost:8000/user/hello").then(r => console.log(r.json()))
+        fetch("http://localhost:8000/user/hello").then(r => ( r.data)).then(data => (console.log(data)))
     }
 
     render() {
